c586ff23ce4bd04a96cc68e65b13482c91dcd302
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-12-09 18:00:21 +0100

    update logic

--- src/app/components/update-liada/update-liada.component.ts
+++ src/app/components/update-liada/update-liada.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LiadesFirebaseService } from '../../services/liades-firebase.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Liada } from '../../models/liada.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-liada',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './update-liada.component.html',
   styleUrl: './update-liada.component.scss'
 })
-export class UpdateLiadaComponent {
+export class UpdateLiadaComponent implements OnInit {
+  incrementForm!: FormGroup;
+  liades: Liada[] = [];
 
+  liadesFirebaseService = inject(LiadesFirebaseService);
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.incrementForm = this.fb.group({
+      selectedLiada: ['', Validators.required], 
+    });
+
+    this.liadesFirebaseService.getLiades().subscribe((liades: Liada[]) => {
+      this.liades = liades;
+    });
+  }
+
+  onSubmit(): void {
+    if (this.incrementForm.valid) {
+      const selectedLiadaId = this.incrementForm.value.selectedLiada;
+      this.liadesFirebaseService.incrementTimes(selectedLiadaId).subscribe({
+        next: () => {
+          console.log('Times incremented successfully!');
+        },
+        error: (err) => {
+          console.error('Error incrementing times:', err);
+        }
+      });
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
 }
--- src/app/services/liades-firebase.service.ts
+++ src/app/services/liades-firebase.service.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, doc, Firestore, increment, updateDoc } from '@angular/fire/firestore';
 import { from, Observable } from 'rxjs';
 import { Liada } from '../models/liada.interface';
-import { addDoc } from 'firebase/firestore';
+import { addDoc, DocumentReference } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +23,14 @@ export class LiadesFirebaseService {
     const toCreateLiada: Liada = { name: name, times: 0 }
     const promise = addDoc(this.liadesCollection, toCreateLiada)
       .then((res) => res.id);
+    return from(promise);
+  }
+
+  incrementTimes(liadaId: string): Observable<void> {
+    const docRef: DocumentReference = doc(this.firestore, 'liades', liadaId);
+    const promise = updateDoc(docRef, {
+      times: increment(1)
+    });
     return from(promise);
   }
 }

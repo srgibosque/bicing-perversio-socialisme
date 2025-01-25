c551d20dd6d6dca0cb75247bd18fa7f52a707a52
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-01-25 11:30:55 +0100

    show navbar when the user is logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {  RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavbarComponent], 
+  imports: [CommonModule, RouterModule, NavbarComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bicing-perversio-socialisme';
+  isUserLoggedIn = false;
+  authStateObs$ = inject(AuthService).authState$;
+
+  ngOnInit() {
+    this.authStateObs$.subscribe((user) => {
+      this.isUserLoggedIn = !!user;
+    });
+  };
 }
